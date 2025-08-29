f4d8dd509119ebbfa439100d1ead5bada14a79b6
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-08-29 14:40:14 +0000

    added 3 icon lists, with text

--- src/components/NewsletterCard.jsx
+++ src/components/NewsletterCard.jsx
@@ -1,4 +1,5 @@
 import mobileIllustration from "../assets/images/illustration-sign-up-mobile.svg";
+import icon from "../assets/images/icon-list.svg";
 
 const NewsletterCard = () => {
   return (
@@ -12,11 +13,33 @@ const NewsletterCard = () => {
       </div>
 
       <div className="mt-10 mx-6">
-        <h1 className="heading-primary-mobile">Stay updated!</h1>
-        <div className="mt-4">
-          <p className="text-mobile">
+        <h1 className="text-preset-mobile">Stay updated!</h1>
+        <div className="my-4">
+          <p className="text-regular">
             Join 60,000+ product managers receiving monthly updates on:
           </p>
+        </div>
+        <div className="mb-10">
+          <div className="flex items-start">
+            <img src={icon} alt="" className="mr-4 mb-2" />
+            <p className="text-preset-regular">
+              Product discovery and building what matters
+            </p>
+          </div>
+
+          <div className="flex items-start">
+            <img src={icon} alt="" className="mr-4 mb-2" />
+            <p>Measuring to ensure updates are a success</p>
+          </div>
+
+          <div className="flex items-start">
+            <img src={icon} alt="" className="mr-4" />
+            <p>And much more!</p>
+          </div>
+        </div>
+
+        <div>
+          <p className="text-preset-3">Email address</p>
         </div>
       </div>
     </div>
