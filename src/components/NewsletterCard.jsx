5708fcdf9e7121ce6f26b06e3156d239d0dda2bc
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-08-29 14:11:21 +0000

    Style mobile heading and text - @layer components - @apply

--- src/components/NewsletterCard.jsx
+++ src/components/NewsletterCard.jsx
@@ -13,6 +13,11 @@ const NewsletterCard = () => {
 
       <div className="mt-10 mx-6">
         <h1 className="heading-primary-mobile">Stay updated!</h1>
+        <div className="mt-4">
+          <p className="text-mobile">
+            Join 60,000+ product managers receiving monthly updates on:
+          </p>
+        </div>
       </div>
     </div>
   );
