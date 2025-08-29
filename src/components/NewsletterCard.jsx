48fd88126b72500b7d1889cac85bf400181bfe50
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-08-29 13:47:36 +0000

    Refactor NewsletterCard component and add CSS styles for card and heading

--- src/components/NewsletterCard.jsx
+++ src/components/NewsletterCard.jsx
@@ -2,16 +2,7 @@ import mobileIllustration from "../assets/images/illustration-sign-up-mobile.svg
 
 const NewsletterCard = () => {
   return (
-    <div
-      className="
-        bg-white 
-        flex flex-col 
-        w-full 
-        max-w-[400px] 
-        mx-auto 
-        mt-[--spacing-600] 
-      "
-    >
+    <div className="card">
       <div>
         <img
           src={mobileIllustration}
@@ -21,12 +12,7 @@ const NewsletterCard = () => {
       </div>
 
       <div className="mt-10 mx-6">
-        <h1
-          className=" text-[--text-preset-1-mobile] leading-[--leading-preset-1-mobile tracking-[--tracking-preset-1-mobile] [font-weight:var(--font-weight-preset-1-mobile)] text-center text-[--color-blue-800]
-  "
-        >
-          Stay updated!
-        </h1>
+        <h1 className="heading-primary-mobile">Stay updated!</h1>
       </div>
     </div>
   );
