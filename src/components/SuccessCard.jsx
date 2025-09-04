98ee4120cb4e34fa67abdf6c48b70a6122104cc6
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-09-04 11:00:21 +0000

    Refactor SuccessCard layout and styles for improved mobile responsiveness

--- src/components/SuccessCard.jsx
+++ src/components/SuccessCard.jsx
@@ -2,18 +2,28 @@ import icon from "../assets/images/icon-list.svg";
 
 const SuccessCard = () => {
   return (
-    <div className="card-success">
-      <img src={icon} alt="Success checkmark" className="w-16 h-16" />
+    <div className="wrapper-mobile">
+      <div className="card-success">
+        <div className=" items-start">
+          <img
+            src={icon}
+            alt="Success checkmark"
+            className="w-16 h-16 shrink-0"
+          />
 
-      <h1 className="text-preset-1-success my-8">Thanks for subscribing!</h1>
+          <h1 className="text-preset-1-success my-8">
+            Thanks for subscribing!
+          </h1>
 
-      <p className="text-preset-regular mb-8 lg:text-left">
-        A confirmation email has been sent to{" "}
-        <strong>ash@loremcompany.com</strong>. Please open it and click the
-        button inside to confirm your subscription.
-      </p>
+          <p className="text-preset-regular mb-8 lg:text-left">
+            A confirmation email has been sent to{" "}
+            <strong>ash@loremcompany.com</strong>. Please open it and click the
+            button inside to confirm your subscription.
+          </p>
 
-      <button className="button">Dismiss message</button>
+          <button className="button">Dismiss message</button>
+        </div>
+      </div>
     </div>
   );
 };
